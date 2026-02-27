3ff3b0429eb93bb14c25dbd17937a90f2001a33d
DiegoA-01 <pinedamontenegrodiegoalejandro@gmail.com>
2026-02-27 15:18:16 -0500

    ACTUALIZO main

--- src/App.java
+++ src/App.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Informacion informacion = new Informacion("Diego", "Pineda", "Calarca", 25);
+        Transaccion transaccion = new Ingreso(125546, "Nequi", 0);
+        
+        int menu;
+        
+        
+
+        do {
+            System.out.println("=====SISTEMA DE FINANZAS FAMILIA======");
+            System.out.println("1:Agregar Saldo.");
+            System.out.println("2:Pago.");
+            System.out.println("3:Consultar saldo");
+            System.out.println("4:Salir.");
+
+            menu = scanner.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Su informacion: " + informacion + " \nIngrese monto a agregar: ");
+                    double ingreso = scanner.nextDouble();
+                    transaccion.calcular(ingreso);
+                    break;
+                case 2:
+                    System.out.println("Su informacion es: " + informacion + " \nIngrese monto a pagar: ");
+                    double pago = scanner.nextDouble();
+                    transaccion.calcular(pago);
+                    break;
+                case 3:
+                    System.out.println("Saldo actual es: " + transaccion.consultarSaldo());
+                    break;
+                default:
+                    System.out.println("Opcion invalida..");
+                    break;
+            }
+        } while (menu !=4);
+        scanner.close();
     }
 }
